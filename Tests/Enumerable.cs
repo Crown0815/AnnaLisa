b82ae0adf33d5d2a62c77711ad828d737851c818
Felix Kröner <felix.kroener@gmx.de>
2020-02-17 20:46:52 +0100

    Improve namings in Enumerable extension tests

--- Tests/Enumerable.cs
+++ Tests/Enumerable.cs
@@ -24,21 +24,21 @@ namespace AnnaLisa.Testing
         [Fact]
         public void with_shared_items_property_returns_shared_property_on_call_to_Shared()
         {
-            var item1 = new { Shared = 1, Distinct = 2 };
-            var item2 = new { Shared = 1, Distinct = 3 };
-            var emptyEnumerable = new []{item1, item2};
+            var item1 = new { Shared = 1, NotShared = 2 };
+            var item2 = new { Shared = 1, NotShared = 3 };
+            var items = new []{item1, item2};
             
-            emptyEnumerable.Shared(x => x.Shared).ShouldBe(1);
+            items.Shared(x => x.Shared).ShouldBe(1);
         }
         
         [Fact]
-        public void with_distinct_items_property_throws_invalid_operation_exception_on_call_to_Shared()
+        public void with_unshared_items_property_throws_invalid_operation_exception_on_call_to_Shared()
         {
-            var item1 = new { Shared = 1, Distinct = 2 };
-            var item2 = new { Shared = 1, Distinct = 3 };
-            var emptyEnumerable = new []{item1, item2};
+            var item1 = new { Shared = 1, NotShared = 2 };
+            var item2 = new { Shared = 1, NotShared = 3 };
+            var items = new []{item1, item2};
             
-            Action callToShared = () => emptyEnumerable.Shared(x => x.Distinct);
+            Action callToShared = () => items.Shared(x => x.NotShared);
 
             callToShared.ShouldThrow<InvalidOperationException>();
         }

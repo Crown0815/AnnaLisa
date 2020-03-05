49d0880f4cc3f2361766f76c55bfddb20c807db3
Felix Kröner <felix.kroener@gmx.de>
2020-03-05 21:22:53 +0100

    Add tests for engineering exponential method  `E`

--- Tests/Extensions/Double.cs
+++ Tests/Extensions/Double.cs
@@ -15,37 +15,37 @@ namespace AnnaLisa.Testing.Extensions
                 yield return integer.E(exponent);
         }
         
-        public static TheoryData<double> WholeNumbersTheoryData()
+        public static TheoryData<double, bool> IsWholeNumberTheoryData()
         {
-            var data = new TheoryData<double>();
-            foreach (var wholeNumber in WholeNumberRange()) 
-                data.Add(wholeNumber);
+            var data = new TheoryData<double, bool>();
+            foreach (var number in WholeNumberRange())
+            {
+                data.Add(number,           true);
+                data.Add(number + Math.PI, false);
+            }
 
             return data;
         }
-        
         [Theory]
-        [MemberData(nameof(WholeNumbersTheoryData))]
-        public void is_whole_number_returns_true_for(double value)
+        [MemberData(nameof(IsWholeNumberTheoryData))]
+        public void is_whole_number_for(double value, bool returns)
         {
-            value.IsWholeNumber().ShouldBeTrue();
-        }
-        
-        
-        public static TheoryData<double> RealNumbersTheoryData()
-        {
-            var data = new TheoryData<double>();
-            foreach (var wholeNumber in WholeNumberRange()) 
-                data.Add(wholeNumber + Math.PI);
-
-            return data;
+            value.IsWholeNumber().ShouldBe(returns);
         }
         
         [Theory]
-        [MemberData(nameof(RealNumbersTheoryData))]
-        public void is_whole_number_returns_false_for(double value)
+        [InlineData(9, 1, 1e9)]
+        [InlineData(8, 2, 2e8)]
+        [InlineData(7, 3, 3e7)]
+        [InlineData(6, 4, 4e6)]
+        [InlineData(5, 5, 5e5)]
+        [InlineData(4, 6, 6e4)]
+        [InlineData(3, 7, 7e3)]
+        [InlineData(2, 8, 8e2)]
+        [InlineData(1, 9, 9e1)]
+        public void E(int exponent, int ofValue, double returns)
         {
-            value.IsWholeNumber().ShouldBeFalse();
+            ofValue.E(exponent).ShouldBe(returns);
         }
     }
 }

cb4da9e47062824edff8bbcb1cd92b9cb57e1272
Felix Kröner <felix.kroener@gmx.de>
2020-03-05 18:14:21 +0100

    Make IsZero an extension method

--- AnnaLisa/Extensions/DoubleExtensions.cs
+++ AnnaLisa/Extensions/DoubleExtensions.cs
@@ -6,6 +6,6 @@ namespace AnnaLisa.Extensions
     {
         public static int Exponent(this double value) => IsZero(value) ? 0 : (int)Math.Log10(value);
 
-        private static bool IsZero(double value) => Math.Abs(value) < double.Epsilon;
+        private static bool IsZero(this double value) => Math.Abs(value) < double.Epsilon;
     }
 }
